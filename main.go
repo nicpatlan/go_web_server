ce91160a04c2a04d33017ad8513ee0b92f193628
Nick Langhorne <nicholaslanghorne@gmail.com>
2024-07-09 08:56:14 -0700

    added an image asset and tested server response.

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	const fileRoot = "."
+	const port = "8080"
+
 	// create server mux and handler
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/", http.FileServer(http.Dir(fileRoot)))
 
 	// create server on localhost port 8080
-	server := http.Server{
-		Addr:    ":8080",
+	server := &http.Server{
+		Addr:    ":" + port,
 		Handler: serveMux,
 	}
 
 	// begin listening and responding to requests
-	server.ListenAndServe()
+	log.Printf("Running server from %s on port: %s", fileRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
