09a94789119d032cff5a446c95c04a2f6be1ee03
Nick Langhorne <nicholaslanghorne@gmail.com>
2024-07-09 10:53:49 -0700

    added handler to respond with server status at /healthz.

--- main.go
+++ main.go
@@ -5,13 +5,29 @@ import (
 	"net/http"
 )
 
+type healthzHandler struct{}
+
+func (healthzHandler) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/healthz" {
+		http.NotFound(wr, req)
+		return
+	}
+	wr.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	wr.WriteHeader(http.StatusOK)
+	wr.Write([]byte("OK"))
+}
+
 func main() {
+	const filePattern = "/app/*"
+	const fileStrip = "/app"
 	const fileRoot = "."
+	const healthzPattern = "/healthz"
 	const port = "8080"
 
 	// create server mux and handler
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(fileRoot)))
+	serveMux.Handle(filePattern, http.StripPrefix(fileStrip, http.FileServer(http.Dir(fileRoot))))
+	serveMux.Handle(healthzPattern, healthzHandler{})
 
 	// create server on localhost port 8080
 	server := &http.Server{
