280e52a136fdb842f3de20cae6216f314c974117
Nick Langhorne <nicholaslanghorne@gmail.com>
2024-07-09 12:22:19 -0700

    added fileHits struct to handle counting/resetting fileserver visitor data and handlers to report.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -17,17 +18,59 @@ func (healthzHandler) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
 	wr.Write([]byte("OK"))
 }
 
+type fileHits struct {
+	fileserverHits int
+}
+
+func (f *fileHits) incrFileHits(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(wr http.ResponseWriter, req *http.Request) {
+		f.fileserverHits++
+		handler.ServeHTTP(wr, req)
+	})
+}
+
+func (f *fileHits) GetHitsHandler(path string) http.Handler {
+	return http.HandlerFunc(func(wr http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != path {
+			http.NotFound(wr, req)
+			return
+		}
+		wr.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		wr.WriteHeader(http.StatusOK)
+		wr.Write([]byte(fmt.Sprintf("Hits: %d", f.fileserverHits)))
+	})
+}
+
+func (f *fileHits) GetResetHandler(path string) http.Handler {
+	return http.HandlerFunc(func(wr http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != path {
+			http.NotFound(wr, req)
+			return
+		}
+		f.fileserverHits = 0
+		wr.WriteHeader(http.StatusOK)
+	})
+}
+
 func main() {
+	// constants
 	const filePattern = "/app/*"
 	const fileStrip = "/app"
 	const fileRoot = "."
 	const healthzPattern = "/healthz"
+	const metricPattern = "/metrics"
+	const resetPattern = "/reset"
 	const port = "8080"
 
-	// create server mux and handler
+	// create server mux handler and fileHits counter
 	serveMux := http.NewServeMux()
-	serveMux.Handle(filePattern, http.StripPrefix(fileStrip, http.FileServer(http.Dir(fileRoot))))
+	fHits := fileHits{}
+
+	// add handlers
+	serveMux.Handle(filePattern, fHits.incrFileHits(http.StripPrefix(fileStrip, http.FileServer(http.Dir(fileRoot)))))
 	serveMux.Handle(healthzPattern, healthzHandler{})
+	serveMux.Handle(metricPattern, fHits.GetHitsHandler(metricPattern))
+	serveMux.Handle(resetPattern, fHits.GetResetHandler(resetPattern))
 
 	// create server on localhost port 8080
 	server := &http.Server{
